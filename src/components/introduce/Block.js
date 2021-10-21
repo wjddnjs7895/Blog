c132e8faa97a02a1a7372628c15970c6c03eff90
wjddnjs7895 <53262722+wjddnjs7895@users.noreply.github.com>
2021-10-21 12:02:57 +0900

    new file

--- src/components/introduce/Block.js
+++ src/components/introduce/Block.js
@@ -1,37 +1,38 @@
 /** @jsxImportSource @emotion/react */
-import React, {useState} from 'react';
+import React from 'react';
 import styled from '@emotion/styled';
-import {animated, useSpring} from 'react-spring';
+import {animated, useSprings, interpolate} from 'react-spring';
 
 import logo_github from '../../assets/introduce/logo_github.png';
 import logo_boj from '../../assets/introduce/logo_boj.png';
 import logo_mbti from '../../assets/introduce/logo_mbti.svg';
 import logo_self from '../../assets/introduce/logo_self.png';
 
-const BlockStyle = styled.div`
+const BlockStyle = styled(animated.div)`
     position : absolute;
     width : 14rem;
-    height : 16rem;
+    height : 16rem;    
+    // top : ${props => props.top || '30vh'};
+    // left :${props => props.left || '30vw'};
     padding-top : 1rem;
     padding-left : 1rem;
     padding-right : 1rem;
     padding-bottom : 5rem;
     background-color : white;
     border : 1px solid rgba(0, 0, 0, 0.3);
-    &:hover {
-        transform : scale(1.2);
-        transition-duration : 1s;
-        z-index : 1;
-    }
+    box-shadow : 3px 6px 12px rgba(0, 0, 0, 0.3);
+    
     transform : scale(1.0);
     transition-duration : 1s;
     transform : rotate(${props => props.degree || '45deg'});
-    box-shadow : 3px 6px 12px rgba(0, 0, 0, 0.3);
-    
-    top : ${props => props.top || '30vh'};
-    left :${props => props.left || '30vw'};
+    &:hover {
+        z-index : 1;
+        transform : scale(1.2);
+        transition-duration : 1s;
+    }
+
     .title {
-        color : black
+        color : black;
     }
 `;
 
@@ -46,7 +47,7 @@ var pos_array = [];
 
 const getRandomPos = (axis) => {
     let min = 10;
-    let max = 60;
+    let max = 1000;
     let temp = parseInt(Math.random() * (max-min) + min);
     if (axis === 0) 
         pos_array.push([0,0]);
@@ -77,68 +78,65 @@ const getRandomDeg = () => {
     return Math.random() * (max - min) + min;
 }
 
+const blocks = [
+    {
+        id : 1,
+        title : '인적사항',
+        content : '이름, 나이, 학교',
+        top : getRandomPos(0),
+        left : getRandomPos(1),
+        degree : String(getRandomDeg())+'deg',
+        src : logo_self,
+     },
+    {
+        id : 2,
+        title : '백준',
+        content : '아이디, 티어',
+        top : getRandomPos(0),
+        left : getRandomPos(1),
+        degree : String(getRandomDeg())+'deg',
+        src : logo_boj,
+    },
+    {
+        id : 3,
+        title : '깃허브',
+        content : '아이디',
+        top : getRandomPos(0),
+        left : getRandomPos(1),
+        degree : String(getRandomDeg())+'deg',
+        src : logo_github,
+    },
+    {
+        id : 4,
+        title : 'MBTI',
+        content : 'ENFP',
+        top : getRandomPos(0),
+        left : getRandomPos(1),
+        degree : String(getRandomDeg())+'deg',
+        src : logo_mbti,
+    },
+];
+
+const to = i => ({x : blocks[i].left, y : blocks[i].top, delay : 1000});
+const from = i => ({x : 0, y : 0})
+
 const Block = (title, content) => {
-    const move = useSpring({
-        from : { x : 0 },
-        to : {x : 300 },
-        config : {duration : 1000}
-    })
-    const [blocks, setBlocks] = useState([
-        {
-            id : 1,
-            title : '인적사항',
-            content : '이름, 나이, 학교',
-            top : String(getRandomPos(0))+'vh',
-            left : String(getRandomPos(1))+'vw',
-            degree : String(getRandomDeg())+'deg',
-            src : logo_self,
-         },
-        {
-            id : 2,
-            title : '백준',
-            content : '아이디, 티어',
-            top : String(getRandomPos(0))+'vh',
-            left : String(getRandomPos(1))+'vw',
-            degree : String(getRandomDeg())+'deg',
-            src : logo_boj,
-        },
-        {
-            id : 3,
-            title : '깃허브',
-            content : '아이디',
-            top : String(getRandomPos(0))+'vh',
-            left : String(getRandomPos(1))+'vw',
-            degree : String(getRandomDeg())+'deg',
-            src : logo_github,
-        },
-        {
-            id : 4,
-            title : 'MBTI',
-            content : 'ENFP',
-            top : String(getRandomPos(0))+'vh',
-            left : String(getRandomPos(1))+'vw',
-            degree : String(getRandomDeg())+'deg',
-            src : logo_mbti,
-        },
-    ]);
+    const [props, set] = useSprings(4, i => ({...to(i), from : from(i)}))
     return (
-        <div>
-            {blocks.map(block => (
-                <animated.div style = {{...move}}>
-                    <BlockStyle 
-                        top = {block.top}
-                        left = {block.left}
-                        key = {block.id}
-                        degree = {block.degree}
-                    >
-                        <PictureStyle src = {block.src}/>
-                        <div className = 'title'>
-                            Title : {block.title}
-                        </div>
-                    </BlockStyle>
-                </animated.div>
-            ))}
-        </div>
+        props.map(({x,y,},i) => (
+            <BlockStyle
+                style = {{ transform: interpolate([x, y],(x , y) => `translate3d(${x}px,${y}px,0)`) }}
+                top = {blocks[i].top}
+                left = {blocks[i].left}
+                key = {blocks[i].id}
+                degree = {blocks[i].degree}
+            >
+                <PictureStyle src = {blocks[i].src}/>
+                    <div className = 'title'>
+                        Title : {blocks[i].title}
+                    </div>
+            </BlockStyle>
+        ))
     );
 }
 
